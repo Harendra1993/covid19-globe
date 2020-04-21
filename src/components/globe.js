196377d8e29092b7b2288c9276f9e70bb17ac19f
patrix <patrix@example.com>
2020-04-21 13:25:01 +0200

    Added new components

---
+++ src/components/globe.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactGlobe from 'react-globe';
+
+const Globe = () => (
+    <div style={{ width: '100vw', height: '100vh' }}>
+        <ReactGlobe />
+    </div>
+)
+
+export default Globe
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react"
-import { Link } from "gatsby"
-import axios from "axios";
 
+// import { Link } from "gatsby"
+import axios from "axios";
+import Globe from "../components/globe";
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
@@ -24,14 +24,16 @@ const IndexPage = () => {
   }, []);
   return (
     < Layout >
-      <SEO title="Home" />
+      {/* <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <Link to="/page-2/">Go to page 2</Link> */}
+
+      <Globe />
     </Layout >
   )
 }
